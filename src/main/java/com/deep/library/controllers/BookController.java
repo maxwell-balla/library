defd4f925c971dba798167feb6b1e8b40b528c23
maxwell-balla <ballamaxwell7@gmail.com>
2024-10-26 14:42:03 +0100

    add feature getAll book

--- src/main/java/com/deep/library/controllers/BookController.java
+++ src/main/java/com/deep/library/controllers/BookController.java
@@ -5,12 +5,12 @@ import com.deep.library.domains.dto.BookResponse;
 import com.deep.library.services.BookService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/books")
@@ -29,5 +29,10 @@ class BookController {
     ) {
         BookResponse newBook = bookService.createBook(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(newBook);
+    }
+
+    @GetMapping
+    Page<BookResponse> getBooks(Pageable pageable) {
+        return bookService.getAllBooks(pageable);
     }
 }
--- src/main/java/com/deep/library/services/BookService.java
+++ src/main/java/com/deep/library/services/BookService.java
@@ -2,7 +2,11 @@ package com.deep.library.services;
 
 import com.deep.library.domains.dto.BookRequest;
 import com.deep.library.domains.dto.BookResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookResponse createBook(BookRequest dto);
+
+    Page<BookResponse> getAllBooks(Pageable pageable);
 }
